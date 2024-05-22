6613ae1cbed1dd5551e34f6eabb00d8c7d0fd0f3
Vinson <12213802@benilde.edu.ph>
2024-05-22 19:17:59 +0800

    added lecture Week3

---
+++ lecture/Week03/01_integers.py
@@ -0,0 +1,12 @@
+class_size = 9
+students_present = 5.1
+student_surnames = [ "Abad", "Hermoza", "Gaza", "Malanog", "Vinson" ]
+appdaet = {
+    "title": "Application Development adn Emerging Technologies",
+    "prerequisite_to": "APPINTR"
+}
+
+print(type(class_size))
+print(type(students_present))
+print(type(student_surnames))
+print(type(appdaet))

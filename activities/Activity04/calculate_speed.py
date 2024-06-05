f787a7ccc9770339938baf96751f674a77ff1c8c
dave34458 <vinsonjoshua56@yahoo.com>
2024-06-05 20:41:36 +0800

    Create calculate_speed.py

---
+++ activities/Activity04/calculate_speed.py
@@ -0,0 +1,9 @@
+distance_in_kilometers = 150
+time_in_hours = 2
+distance_in_miles = distance_in_kilometers/1.6
+distance_in_meters = distance_in_kilometers*1000
+time_in_seconds = 2*60*60
+print(f'The speed in kilometers per hour is: {distance_in_kilometers/time_in_hours}')
+print(f'The speed in miles per hour is: {distance_in_miles/time_in_hours}')
+print(f'The speed in meters per second is: {distance_in_meters/time_in_seconds}\n')
+

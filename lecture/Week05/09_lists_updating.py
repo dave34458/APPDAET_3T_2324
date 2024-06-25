8fbc784121c69b30aa2fbc20e9498f4ab66aa457
dave34458 <149953914+dave34458@users.noreply.github.com>
2024-06-26 04:01:18 +0800

    Add files via upload

---
+++ lecture/Week05/09_lists_updating.py
@@ -0,0 +1,11 @@
+car_makes = ["Toyota", "Mitsubishi", "Honda", "Nissan"]
+print(car_makes)
+
+car_makes[2] = "Mazda"
+print(car_makes)
+
+car_makes.append("Honda")
+print(car_makes)
+
+car_makes.pop(2)
+print(car_makes)
